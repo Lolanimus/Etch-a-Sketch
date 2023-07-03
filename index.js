06e8610256fb0b82bbc55157cf0b6e33f53df5b9
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2023-07-03 10:31:58 +0800

    Now divs can change its color if the user's mouse enters the zone of the div

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ firstDiv.className = "first";
 document.body.appendChild(firstDiv)
 
 let vertDiv = [];
-
 for (let i = 0; i < 16; i++) {
     vertDiv[i] = document.createElement('div');        
     firstDiv.appendChild(vertDiv[i]);
 }
+
+vertDiv.forEach(e => {
+    e.addEventListener('mouseenter', () => {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        e.style.backgroundColor = color;
+        e.className = 'enter';
+    });
+});
