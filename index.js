97bc67d2e26af81fb1a415702f0e34641352a37c
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2023-07-03 13:41:30 +0800

    Now the user can select how many squares in the grid there will be

--- index.js
+++ index.js
@@ -2,20 +2,44 @@ const firstDiv = document.createElement('div');
 firstDiv.className = "first";
 document.body.appendChild(firstDiv)
 
+const button = document.createElement('button');
+button.textContent = "Create new grid?";
+document.body.appendChild(button);
+
+let maxDivs = 16;
 let vertDiv = [];
-for (let i = 0; i < 16; i++) {
-    vertDiv[i] = document.createElement('div');        
-    firstDiv.appendChild(vertDiv[i]);
+
+function createGrid() {
+    for (let i = 0; i < maxDivs; i++) {
+        vertDiv[i] = document.createElement('div');        
+        firstDiv.appendChild(vertDiv[i]);
+        let flexBas = 100/Math.sqrt(maxDivs);
+        vertDiv[i].style.flexBasis = flexBas.toString() + '%';
+    }
+    
+    vertDiv.forEach(e => {
+        e.addEventListener('mouseenter', () => {
+            let letters = '0123456789ABCDEF';
+            let color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            e.style.backgroundColor = color;
+            e.className = 'enter';
+        });
+    });
 }
 
-vertDiv.forEach(e => {
-    e.addEventListener('mouseenter', () => {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        e.style.backgroundColor = color;
-        e.className = 'enter';
-    });
-});
+createGrid();
+
+button.addEventListener('click', () => {
+    let squares = prompt("How much squares do you want to generate for a new grid(Max 100)?", 16);
+    if(squares > 100) {
+        maxDivs = 100;
+    }
+    else {
+        maxDivs = squares;
+    }
+    firstDiv.replaceChildren();
+    createGrid();
+})
